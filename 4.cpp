d1fd0cbd10fa8c57a97d897fa0480caf47f082e8
BishayuG <888ghimirebishayu@gmail.com>
2025-02-23 07:33:18 +0545

    Add files via upload

--- 4.cpp
+++ 4.cpp
@@ -1,42 +1,17 @@
-// Write a C++ program to create a class called Triangle that has private member variables for the lengths of its three sides. Implement member functions to determine if the triangle is equilateral, isosceles, or scalene.
+// Write a C++ program to reverse a string using recursion.
 #include <iostream>
+#include <string>
 using namespace std;
 
-class Triangle {
-private:
-    double side1, side2, side3;
-
-public:
-    Triangle(double s1, double s2, double s3)
-        : side1(s1), side2(s2), side3(s3) {}
-
-    bool isValid() {
-        if (side1 + side2 > side3 &&
-            side2 + side3 > side1 &&
-            side3 + side1 > side2) {
-            return true;
-        }
-        return false;
-    }
-
-    bool isEquilateral() {
-        return (side1 == side2 && side2 == side3 && isValid());
-    }
-
-    bool isIsosceles() {
-        return ( (side1 == side2 || side2 == side3 || side3 == side1) && isValid() && !isEquilateral());
-    }
-
-    bool isScalene() {
-        return (side1 != side2 && side2 != side3 && side3 != side1 && isValid());
-    }
-};
+string reverseString(const string &s) {
+    if (s.empty()) return s;
+    return reverseString(s.substr(1)) + s[0];
+}
 
 int main() {
-    Triangle t1(3, 3, 3);
-    cout << "Is Equilateral? " << boolalpha << t1.isEquilateral() << endl;
-    cout << "Is Isosceles? "   << boolalpha << t1.isIsosceles()   << endl;
-    cout << "Is Scalene? "    << boolalpha << t1.isScalene()     << endl;
-
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    cout << "Reversed string: " << reverseString(str) << endl;
     return 0;
 }
--- 7.cpp
+++ 7.cpp
@@ -1,14 +1,26 @@
+// Write a C++ program to implement a recursive function to find the sum of all prime numbers in a given range.
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    int factorial = 1;
-    for(int i = 1; i <= n; i++){
-        factorial *= i;
-    }
-    cout << "The factorial of " << n << " is: " << factorial << endl;
+bool isPrime(int num, int i = 2) {
+    if (num < 2) return false;
+    if (i * i > num) return true;
+    if (num % i == 0) return false;
+    return isPrime(num, i + 1);
+}
+
+int sumPrimes(int start, int end) {
+    if (start > end) return 0;
+    return (isPrime(start) ? start : 0) + sumPrimes(start + 1, end);
+}
+
+int main() {
+    int s, e;
+    cout << "Enter start of range: ";
+    cin >> s;
+    cout << "Enter end of range: ";
+    cin >> e;
+
+    cout << "Sum of prime numbers in the range: " << sumPrimes(s, e) << endl;
     return 0;
 }
