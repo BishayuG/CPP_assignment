7893d6b53ca7ff668e865a08137cc94d9df7fc0e
BishayuG <888ghimirebishayu@gmail.com>
2025-02-23 07:31:49 +0545

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,12 +1,39 @@
+// Write a C++ program to implement a class called Employee that has private member variables for name, employee ID, and salary. Include member functions to calculate and set salary based on employee performance.
 #include <iostream>
+#include <string>
 using namespace std;
 
+class Employee {
+private:
+    string name;
+    int employeeID;
+    double salary;
+
+public:
+    Employee(string n, int id, double sal)
+        : name(n), employeeID(id), salary(sal) {}
+
+    void setPerformance(int rating) {
+        if(rating >= 9) {
+            salary *= 1.30;
+        } else if(rating >= 6) {
+            salary *= 1.10;
+        } else {
+            salary *= 1.02;
+        }
+    }
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Employee ID: " << employeeID << endl;
+        cout << "Salary: " << salary << endl;
+    }
+};
+
 int main() {
-    string firstName, lastName;
-    cout << "Enter your first name and last name : ";
-    cin >> firstName >> lastName;
-    cout << "First Name : " << firstName << endl;
-    cout << "Last Name : " << lastName << endl;
-    cout << "Name in reverse is: " << lastName << " " << firstName << endl;
+    Employee emp("John Doe", 1234, 50000);
+    emp.setPerformance(8);
+    emp.display();
+
     return 0;
 }
